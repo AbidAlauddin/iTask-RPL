023bc51a19e1e50b1a75bad822ac019286be1017
ab1dal <96287687+ab1dal@users.noreply.github.com>
2025-03-04 17:55:04 +0700

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<x-layout title="Home">
+    <div class="flex justify-center w-full">
+        <ul class="list-disc">
+            <li>Create a list</li>
+            <li>Add a task</li>
+            <li>Set a deadline</li>
+            <li>Mark complete once completed</li>
+        </ul>
+    </div>
+    <p class="pt-16 w-full text-center">“Everything should be made as simple as possible, but not simpler.”</p>
+</x-layout>
